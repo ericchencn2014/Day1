ad28d5bc4958f25887a699254c3d1aed6d6e3175
ericchencn2014 <ericchencn@outlook.com>
2014-12-01 23:11:39 +0000

    Create PlayBinary.rb

---
+++ PlayBinary.rb
@@ -0,0 +1,44 @@
+require_relative 'numbergame'
+
+class PlayBinary < NumberGame
+
+  def set_num
+    @num = (@lower+@upper)/2
+  end
+
+  def apply_guessing_method(result)
+    if result == :less_than
+      @upper = @num-1
+    elsif result == :greater_than
+      @lower = @num+1
+    end
+    @num=(@lower+@upper)/2
+    p "upper #{@upper}"
+    p "lower #{@lower}"
+    p "num #{@num}"
+  end
+
+=begin
+  def guess_number(player_obj, lower, upper)
+    num = (lower+upper)/2
+    player_obj.num_attempts+=1
+    while ((result = player_obj.oracle.is_this_the_number?(num)) != :correct) && (player_obj.num_attempts <= player_obj.max_num_attempts) do
+      # puts "#{__method__}:I guessed #{num}"
+      if result == :less_than
+        upper = num-1
+      elsif result == :greater_than
+        lower = num+1
+      end
+      num=(lower+upper)/2
+      player_obj.num_attempts+=1
+    end
+    if (player_obj.num_attempts <= player_obj.max_num_attempts)
+      # puts "#{__method__}: Yippee, I guessed #{num} and won!"
+      :success
+    else
+      :fail
+    end
+  end
+=end
+
+end
